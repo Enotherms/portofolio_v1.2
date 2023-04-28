919f02a5e1ce984aa554ba8b5ae3fbe1ecf8e1d7
Enotherms <sayabiyyu@gmail.com>
2023-04-28 21:21:37 +0700

    modified:   src/components/Navbar.jsx 	modified:   src/components/Skill.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import React, {useEffect } from 'react'
 import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(true)
-
     useEffect(() => {
             const checkbox = document.querySelector('#toggle')
             const html = document.querySelector('html')
@@ -17,36 +14,24 @@ const Navbar = () => {
             return () => checkbox.removeEventListener('click', handleCheckboxClick)
         }, [])
 
-    const handleNav = () => {
-        setNav(!nav)
-    }
-
     return (
-        <div id className=' bg-slate-200 dark:text-gray-200 dark:bg-slate-600 overflow-hidden h-16 uppercase text-gray-900 bg-primary-100 top-0 px-[30px] flex items-start justify-between max-w-full mx-auto z-100 xl:items-center rounded-b-md'>
-            <div className='flex items-center p-4'>
-                <span className='mr-2 font-semibold'>Light</span>
-                    <input type='checkbox' id='toggle' className='hidden' />
-                    <label for='toggle'>
-                        <div className='w-9 h-5 bg-slate-300 rounded-full flex items-center p-1 cursor-pointer'>
-                            <div className='w-4 h-4 bg-slate-50 rounded-full toggle-circle'></div>
-                        </div>
-                    </label>
-                <span className='ml-2 font-semibold'>Dark</span>
+        <div id className='bg-slate-200 dark:text-gray-200 dark:bg-slate-600 overflow-hidden h-16 uppercase text-gray-900 bg-primary-100 top-0 px-[30px] flex items-start justify-between max-w-full mx-auto z-100 xl:items-center rounded-b-md'>
+    <div className='flex items-center p-4'>
+        <span className='mr-2 font-semibold'>Light</span>
+        <input type='checkbox' id='toggle' className='hidden' />
+        <label for='toggle'>
+            <div className='w-9 h-5 bg-slate-300 rounded-full flex items-center p-1 cursor-pointer'>
+                <div className='w-4 h-4 bg-slate-50 rounded-full toggle-circle'></div>
             </div>
-            <ul className=' hidden md:flex font-semibold'>
+        </label>
+        <span className='ml-2 font-semibold'>Dark</span>
+        </div>
+            <ul className='items-center p-4 font-semibold'>
                 <Link to="contact" spy={true} smooth={true} duration={400} className="cursor-pointer p-4 hover:text-gray-900 hover:bg-gray-300 hover:rounded">Contact</Link>
                 <Link to="skill" spy={true} smooth={true} duration={400} className="cursor-pointer p-4 hover:text-gray-900 hover:bg-gray-300 hover:rounded">About</Link>
             </ul>
-            <div onClick={handleNav} className='block md:hidden'>
-                {!nav ? <AiOutlineClose size={25}/> : <AiOutlineMenu size={25}/>}
-            </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] dark:text-gray-200 dark:bg-gray-900 rounded-r-xl shadow mb-4 bg-white  h-full ease-in-out duration-500' : 'fixed Right-0 top-0 w-[60%] dark:text-gray-200 dark:bg-gray-900 rounded-r-xl shadow mb-4 bg-white h-full ease-in-out duration-500 left-[-100%]'}>
-                <ul className='p-4 uppercase flex flex-col'>
-                <Link to="contact" spy={true} smooth={true} duration={400} className="cursor-pointer p-4 hover:text-gray-900 hover:bg-gray-300 hover:rounded">Contact</Link>
-                <Link to="skill" spy={true} smooth={true} duration={400} className="cursor-pointer p-4 hover:text-gray-900 hover:bg-gray-300 hover:rounded">About</Link>
-                </ul>
-            </div>
-        </div>
+    </div>
+
     )
 }
 
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -9,7 +9,7 @@ export const Skill = () => {
             </div>
             <p className='mx-7 mt-3 mb-8 text-center text-xl font-semiboldbold text-gray-900 dark:bg-slate-600 dark:text-slate-200'>Some kills that I have acquired from my experience</p>
             <div className='container mx-auto px-8 font-inter sm:flex sm:flex-wrap sm:gap-6 sm:justify-evenly '>
-                <div className='grid grid-cols-1 sm:grid-cols-3 gap-20'>
+                <div className='grid grid-cols-1 sm:grid-cols-3 gap-16'>
                     <div className='rounded-md shadow-lg overflow-hidden mb-10 bg-white dark:bg-gray-900 sm:mb-auto sm:w-64'>
                         <img className='w-full' src={process.env.PUBLIC_URL + '/img/logo/javascript.png'} alt='javascript Icon' />
                     </div>
